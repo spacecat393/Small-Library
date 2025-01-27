e05e96278c082bd8ea391cf9aace33ed9cabb788
nali <jjcakecat@gmail.com>
2025-01-27 09:35:46 +0700

    fix tr

--- src/main/java/com/nali/small/mixin/MixinEntityRenderer.java
+++ src/main/java/com/nali/small/mixin/MixinEntityRenderer.java
@@ -1,223 +1,220 @@
-package com.nali.small.mixin;
-
-import com.nali.render.RenderO;
-import com.nali.small.Small;
-import com.nali.small.draw.Draw;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.EntityRenderer;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL14;
-import org.lwjgl.opengl.GL20;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-@Mixin(EntityRenderer.class)
-public abstract class MixinEntityRenderer
-{
-//	@Shadow @Final private Minecraft mc;
+//package com.nali.small.mixin;
 //
-//	@Shadow public abstract void disableLightmap();
+//import com.nali.render.RenderO;
+//import com.nali.small.Small;
+//import com.nali.small.draw.Draw;
+//import net.minecraft.client.renderer.EntityRenderer;
+//import org.lwjgl.opengl.GL11;
+//import org.lwjgl.opengl.GL14;
+//import org.lwjgl.opengl.GL20;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 //
-////	private static byte BIT;//mix tr
-//
-//	private static float PARTIALTICKS;
-//	private static Frustum FRUSTUM;
-//
-//	@Redirect(method = "renderWorldPass", at = @At(value = "NEW", target = "()Lnet/minecraft/client/renderer/culling/Frustum;"))
-//	private Frustum nali_small_renderWorldPassIC()
-//	{
-//		FRUSTUM = new Frustum();
-//		return FRUSTUM;
-//	}
-//
-//	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.AFTER, ordinal = 2))
-//	private void nali_small_renderWorldPassA(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
-//	{
-//		PARTIALTICKS = partialTicks;
-////		RenderO.take();
-////		GL20.glBlendEquationSeparate(GL14.GL_FUNC_ADD, GL14.GL_FUNC_ADD);
-////		GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+//@Mixin(EntityRenderer.class)
+//public abstract class MixinEntityRenderer
+//{
+////	@Shadow @Final private Minecraft mc;
 ////
-////		if (!Draw.MODEL_MAP.isEmpty())
-////		{
-////			Draw.draw(Draw.MODEL_MAP);
-////		}
+////	@Shadow public abstract void disableLightmap();
 ////
-////		if ((BIT & 1) == 0)
-////		{
-////			if (!Draw.TRANSLUCENT_MAP.isEmpty())
-////			{
-//////			draw(TRANSLUCENT_MAP);
-////				Draw.drawT();
-////			}
+//////	private static byte BIT;//mix tr
 ////
-//////			Draw.run();
-////		}
+////	private static float PARTIALTICKS;
+////	private static Frustum FRUSTUM;
 ////
-////		RenderO.free();
-////		Draw.clear();
+////	@Redirect(method = "renderWorldPass", at = @At(value = "NEW", target = "()Lnet/minecraft/client/renderer/culling/Frustum;"))
+////	private Frustum nali_small_renderWorldPassIC()
+////	{
+////		FRUSTUM = new Frustum();
+////		return FRUSTUM;
+////	}
 ////
+////	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.AFTER, ordinal = 2))
+////	private void nali_small_renderWorldPassA(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
+////	{
+////		PARTIALTICKS = partialTicks;
 //////		RenderO.take();
+//////		GL20.glBlendEquationSeparate(GL14.GL_FUNC_ADD, GL14.GL_FUNC_ADD);
+//////		GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
 //////
 //////		if (!Draw.MODEL_MAP.isEmpty())
 //////		{
 //////			Draw.draw(Draw.MODEL_MAP);
 //////		}
 //////
-//////		//model texture
-////////		if (!E_GLOW_MAP.isEmpty())
+//////		if ((BIT & 1) == 0)
+//////		{
+//////			if (!Draw.TRANSLUCENT_MAP.isEmpty())
+//////			{
+////////			draw(TRANSLUCENT_MAP);
+//////				Draw.drawT();
+//////			}
+//////
+////////			Draw.run();
+//////		}
+//////
+//////		RenderO.free();
+//////		Draw.clear();
+//////
+////////		RenderO.take();
+////////
+////////		if (!Draw.MODEL_MAP.isEmpty())
 ////////		{
-////////			draw(E_GLOW_MAP);
+////////			Draw.draw(Draw.MODEL_MAP);
+////////		}
+////////
+////////		//model texture
+//////////		if (!E_GLOW_MAP.isEmpty())
+//////////		{
+//////////			draw(E_GLOW_MAP);
+//////////		}
+////////
+////////		Draw.clear();
+////////		RenderO.free();
+////	}
+////
+//////	@Redirect(method = "renderWorldPass", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/EntityRenderer;debugView:Z", ordinal = 0))
+//////	private boolean nali_small_renderWorldPass0(EntityRenderer instance)
+//////	{
+//////////		ICamera icamera = new Frustum();
+////////		if ((Small.FLAG & 1) == 0)
+////////		{
+////////			ForgeHooksClient.setRenderPass(0);
+////////			Entity entity = this.mc.getRenderViewEntity();
+////////			RenderGlobal renderglobal = this.mc.renderGlobal;
+////////			GlStateManager.matrixMode(5888);
+////////			GlStateManager.popMatrix();
+////////			GlStateManager.pushMatrix();
+////////			RenderHelper.enableStandardItemLighting();
+////////			renderglobal.renderEntities(entity, FRUSTUM, PARTIALTICKS);
+////////			RenderHelper.disableStandardItemLighting();
+////////			this.disableLightmap();
+////////
+////////			RenderO.take();
+////////			GL20.glBlendEquationSeparate(GL14.GL_FUNC_ADD, GL14.GL_FUNC_ADD);
+////////			GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+////////
+////////			if (!Draw.MODEL_MAP.isEmpty())//mixin to own render
+////////			{
+////////				//bindtex 0
+//////////				GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_MODULATE);
+////////				//bindtex 1
+//////////				GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_ADD);
+////////
+////////				//when use default shader
+//////////				GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
+//////////				GL11.glEnableClientState(GL11.GL_NORMAL_ARRAY);
+//////////				GL11.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
+//////////
+//////////				int stride = 8 * Float.BYTES;
+//////////				GL11.glVertexPointer(3, GL11.GL_FLOAT, stride, 0);
+//////////				GL11.glNormalPointer(GL11.GL_FLOAT, stride, 3 * Float.BYTES);
+//////////				GL11.glTexCoordPointer(2, GL11.GL_FLOAT, stride, 6 * Float.BYTES);
+////////
+////////				Draw.draw(Draw.MODEL_MAP);
+////////			}
+////////
+////////			if ((Small.FLAG & 2) == 0)
+////////			{
+////////				if (!Draw.TRANSLUCENT_MAP.isEmpty())
+////////				{
+////////					Draw.draw(Draw.TRANSLUCENT_MAP);
+////////		//			Draw.drawT();
+////////				}
+////////
+////////	//			Draw.run();
+////////			}
+////////
+////////			RenderO.free();
+////////			Draw.clear();
+////////	//			Display.update();
 ////////		}
 //////
-//////		Draw.clear();
-//////		RenderO.free();
-//	}
+//////		return true;
+//////	}
+////
+////	@Shadow @Final private Minecraft mc;
 //
-////	@Redirect(method = "renderWorldPass", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/EntityRenderer;debugView:Z", ordinal = 0))
-////	private boolean nali_small_renderWorldPass0(EntityRenderer instance)
-////	{
-////////		ICamera icamera = new Frustum();
-//////		if ((Small.FLAG & 1) == 0)
-//////		{
-//////			ForgeHooksClient.setRenderPass(0);
-//////			Entity entity = this.mc.getRenderViewEntity();
-//////			RenderGlobal renderglobal = this.mc.renderGlobal;
+//	////	//use only 0
+//////	@Redirect(method = "renderWorldPass", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/EntityRenderer;debugView:Z", ordinal = 2))
+//////	private boolean nali_small_renderWorldPass1(EntityRenderer instance)
+//////	{
+//////		return true;
+//////	}
+//
+////	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.AFTER, ordinal = 3))
+////	private void nali_small_renderWorldPassB(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
+//	//draw before get pos
+//	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.BEFORE, ordinal = 0))
+////	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;disableAlpha()V", shift = At.Shift.AFTER, ordinal = 0))
+////	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/renderer/culling/ICamera;F)V", shift = At.Shift.AFTER, ordinal = 0))
+//	private void nali_small_renderWorldPass(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
+//	{
+////		if ((Small.FLAG & 1) == 1)
+////		{
+////			ForgeHooksClient.setRenderPass(0);
+////			Entity entity = this.mc.getRenderViewEntity();
+////			RenderGlobal renderglobal = this.mc.renderGlobal;
 //////			GlStateManager.matrixMode(5888);
 //////			GlStateManager.popMatrix();
 //////			GlStateManager.pushMatrix();
-//////			RenderHelper.enableStandardItemLighting();
-//////			renderglobal.renderEntities(entity, FRUSTUM, PARTIALTICKS);
-//////			RenderHelper.disableStandardItemLighting();
+////			RenderHelper.enableStandardItemLighting();
+////			renderglobal.renderEntities(entity, FRUSTUM, PARTIALTICKS);
+////			GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+////			RenderHelper.disableStandardItemLighting();
 //////			this.disableLightmap();
-//////
-//////			RenderO.take();
-//////			GL20.glBlendEquationSeparate(GL14.GL_FUNC_ADD, GL14.GL_FUNC_ADD);
-//////			GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
-//////
-//////			if (!Draw.MODEL_MAP.isEmpty())//mixin to own render
-//////			{
-//////				//bindtex 0
-////////				GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_MODULATE);
-//////				//bindtex 1
-////////				GL11.glTexEnvi(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE, GL11.GL_ADD);
-//////
-//////				//when use default shader
-////////				GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-////////				GL11.glEnableClientState(GL11.GL_NORMAL_ARRAY);
-////////				GL11.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
-////////
-////////				int stride = 8 * Float.BYTES;
-////////				GL11.glVertexPointer(3, GL11.GL_FLOAT, stride, 0);
-////////				GL11.glNormalPointer(GL11.GL_FLOAT, stride, 3 * Float.BYTES);
-////////				GL11.glTexCoordPointer(2, GL11.GL_FLOAT, stride, 6 * Float.BYTES);
-//////
-//////				Draw.draw(Draw.MODEL_MAP);
-//////			}
-//////
-//////			if ((Small.FLAG & 2) == 0)
-//////			{
-//////				if (!Draw.TRANSLUCENT_MAP.isEmpty())
-//////				{
-//////					Draw.draw(Draw.TRANSLUCENT_MAP);
-//////		//			Draw.drawT();
-//////				}
-//////
-//////	//			Draw.run();
-//////			}
-//////
-//////			RenderO.free();
-//////			Draw.clear();
-//////	//			Display.update();
-//////		}
+//////			ForgeHooksClient.setRenderPass(-1);
 ////
-////		return true;
-////	}
+////		GlStateManager.enableAlpha();
+//		RenderO.take();
+//		GL11.glEnable(GL11.GL_DEPTH_TEST);
+//		GL11.glEnable(GL11.GL_BLEND);
+//		GL20.glBlendEquationSeparate(GL14.GL_FUNC_ADD, GL14.GL_FUNC_ADD);
+//		GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
 //
-	@Shadow @Final private Minecraft mc;
-
-	////	//use only 0
-////	@Redirect(method = "renderWorldPass", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/EntityRenderer;debugView:Z", ordinal = 2))
-////	private boolean nali_small_renderWorldPass1(EntityRenderer instance)
-////	{
-////		return true;
-////	}
-
-//	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.AFTER, ordinal = 3))
-//	private void nali_small_renderWorldPassB(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
-	//draw before get pos
-//	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.BEFORE, ordinal = 0))
-	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;disableAlpha()V", shift = At.Shift.AFTER, ordinal = 0))
-//	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/renderer/culling/ICamera;F)V", shift = At.Shift.AFTER, ordinal = 0))
-	private void nali_small_renderWorldPass(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
-	{
-//		if ((Small.FLAG & 1) == 1)
+//		if (!Draw.MODEL_MAP.isEmpty())
 //		{
-//			ForgeHooksClient.setRenderPass(0);
-//			Entity entity = this.mc.getRenderViewEntity();
-//			RenderGlobal renderglobal = this.mc.renderGlobal;
-////			GlStateManager.matrixMode(5888);
-////			GlStateManager.popMatrix();
-////			GlStateManager.pushMatrix();
-//			RenderHelper.enableStandardItemLighting();
-//			renderglobal.renderEntities(entity, FRUSTUM, PARTIALTICKS);
-//			GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-//			RenderHelper.disableStandardItemLighting();
-////			this.disableLightmap();
-////			ForgeHooksClient.setRenderPass(-1);
-//
-//		GlStateManager.enableAlpha();
-		RenderO.take();
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		GL11.glEnable(GL11.GL_BLEND);
-		GL20.glBlendEquationSeparate(GL14.GL_FUNC_ADD, GL14.GL_FUNC_ADD);
-		GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
-
-		if (!Draw.MODEL_MAP.isEmpty())
-		{
-			Draw.draw(Draw.MODEL_MAP);
-		}
-
-//		if ((Small.FLAG & 2) == 0)
-		if ((Small.FLAG & 1) == 0)
-		{
-			if (!Draw.TRANSLUCENT_MAP.isEmpty())
-			{
-//					Draw.draw(Draw.TRANSLUCENT_MAP);
-				Draw.drawT();
-			}
-		}
-
-		RenderO.free();
-		Draw.clear();
-//			Draw.run();
-//			Display.update();
+//			Draw.draw(Draw.MODEL_MAP);
 //		}
-
+//
+////		if ((Small.FLAG & 2) == 0)
+//		if ((Small.FLAG & 1) == 0)
+//		{
+//			if (!Draw.TRANSLUCENT_MAP.isEmpty())
+//			{
+////					Draw.draw(Draw.TRANSLUCENT_MAP);
+//				Draw.drawT();
+//			}
+//		}
+//
+//		RenderO.free();
 //		Draw.clear();
-//		Small.FLAG ^= 1/* | 2*/;
-	}
-
-//	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.BEFORE, ordinal = 3))
-//	private void nali_renderWorldPass1(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
-//	{
-//		Draw.run();
-/// /		RenderO.take();
-/// /		if (!Draw.TRANSLUCENT_MAP.isEmpty())
-/// /		{
-/// ///			GL11.glDepthMask(false);
-/// /			Draw.draw(Draw.TRANSLUCENT_MAP);
-/// /		}
-/// /		RenderO.free();
-/// /		Draw.clear();
+////			Draw.run();
+////			Display.update();
+////		}
+//
+////		Draw.clear();
+////		Small.FLAG ^= 1/* | 2*/;
 //	}
-
-//	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.BEFORE, ordinal = 0))
-//	private void nali_renderWorldPass1(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
-//	{
-//	}
-}
+//
+////	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.BEFORE, ordinal = 3))
+////	private void nali_renderWorldPass1(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
+////	{
+////		Draw.run();
+///// /		RenderO.take();
+///// /		if (!Draw.TRANSLUCENT_MAP.isEmpty())
+///// /		{
+///// ///			GL11.glDepthMask(false);
+///// /			Draw.draw(Draw.TRANSLUCENT_MAP);
+///// /		}
+///// /		RenderO.free();
+///// /		Draw.clear();
+////	}
+//
+////	@Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/BlockRenderLayer;DILnet/minecraft/entity/Entity;)I", shift = At.Shift.BEFORE, ordinal = 0))
+////	private void nali_renderWorldPass1(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci)
+////	{
+////	}
+//}
--- src/main/java/com/nali/small/mixin/MixinRenderGlobal.java
+++ src/main/java/com/nali/small/mixin/MixinRenderGlobal.java
@@ -1,50 +1,215 @@
-//package com.nali.small.mixin;
+package com.nali.small.mixin;
+
+import com.nali.render.RenderO;
+import com.nali.small.Small;
+import com.nali.small.draw.Draw;
+import com.nali.small.entity.IMixE;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.culling.ICamera;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.util.ClassInheritanceMultiMap;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
+import org.lwjgl.opengl.GL20;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@Mixin(value = RenderGlobal.class, priority = Integer.MAX_VALUE)
+@Mixin(RenderGlobal.class)
+public abstract class MixinRenderGlobal
+{
+	@Shadow @Final private Minecraft mc;
+	private static int LAST;
+	private static ClassInheritanceMultiMap<Entity>[] ENTITY_CLASSINHERITANCEMULTIMAP_ARRAY;
+	private static ClassInheritanceMultiMap<Entity>[] NEW_ENTITY_CLASSINHERITANCEMULTIMAP_ARRAY = new ClassInheritanceMultiMap[1];
+
+	@Redirect(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos;getY()I"))
+	private int nali_extra_renderEntities(BlockPos instance)
+	{
+		ClassInheritanceMultiMap<Entity> entity_classinheritancemultimap = ENTITY_CLASSINHERITANCEMULTIMAP_ARRAY[instance.getY() / 16];
+
+		List<Entity> small_entity_list = new ArrayList();
+		List<Entity> entity_list = new ArrayList();
+		List<Entity> translucent_entity_list = new ArrayList();
+
+		if (!entity_classinheritancemultimap.isEmpty())
+		{
+			for (Entity entity : entity_classinheritancemultimap)
+			{
+				if (entity instanceof IMixE)
+				{
+					small_entity_list.add(entity);
+				}
+				else if (entity.isInvisible() || entity instanceof EntitySlime/* || entity.isInvisibleToPlayer(Minecraft.getMinecraft().player)*/)
+				{
+//					if ((Small.FLAG & 1) == 0)
+//					{
+					translucent_entity_list.add(entity);
+//					}
+				}
+				else
+				{
+					entity_list.add(entity);
+				}
+			}
+		}
+
+		ClassInheritanceMultiMap<Entity> new_entity_classinheritancemultimap = new ClassInheritanceMultiMap(Entity.class);
+		//s-small
+		for (Entity entity : small_entity_list)
+		{
+			new_entity_classinheritancemultimap.add(entity);
+		}
+
+		int size = small_entity_list.size();
+		if (size > 0)
+		{
+			LAST = small_entity_list.get(size - 1).getEntityId();
+		}
+		else
+		{
+			LAST = -1;
+		}
+		//e-small
+
+		for (Entity entity : entity_list)
+		{
+			new_entity_classinheritancemultimap.add(entity);
+		}
+
+		//s-sort
+		Entity view_entity = this.mc.getRenderViewEntity();
+		size = translucent_entity_list.size() - 1;
+		for (int x = 0; x < size; ++x)
+		{
+			for (int y = 0; y < size; ++y)
+			{
+				int y1 = y + 1;
+				Entity y_entity = translucent_entity_list.get(y);
+				Entity y1_entity = translucent_entity_list.get(y1);
+				if (view_entity.getDistanceSq(y_entity) < view_entity.getDistanceSq(y1_entity))
+				{
+					translucent_entity_list.set(y, y1_entity);
+					translucent_entity_list.set(y1, y_entity);
+				}
+			}
+		}
+		//e-sort
+
+		for (Entity entity : translucent_entity_list)
+		{
+			new_entity_classinheritancemultimap.add(entity);
+		}
+
+		NEW_ENTITY_CLASSINHERITANCEMULTIMAP_ARRAY[0] = new_entity_classinheritancemultimap;
+		return 0;
+	}
+
+	@Redirect(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/Chunk;getEntityLists()[Lnet/minecraft/util/ClassInheritanceMultiMap;"))
+	private ClassInheritanceMultiMap<Entity>[] nali_extra_renderEntities(Chunk chunk)
+	{
+		ENTITY_CLASSINHERITANCEMULTIMAP_ARRAY = chunk.getEntityLists();
+		return NEW_ENTITY_CLASSINHERITANCEMULTIMAP_ARRAY;
+	}
+
+	@Redirect(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderManager;shouldRender(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/renderer/culling/ICamera;DDD)Z"))
+	private boolean nali_extra_renderEntities(RenderManager renderManager, Entity entity2, ICamera camera, double camX, double camY, double camZ)
+	{
+		if (LAST != -3)
+		{
+			if (LAST == entity2.getEntityId())
+			{
+				LAST = -2;
+			}
+		}
+
+		return renderManager.shouldRender(entity2, camera, camX, camY, camZ) || entity2.isRidingOrBeingRiddenBy(this.mc.player);
+	}
+
+//	//always true
+//	@Redirect(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;isBlockLoaded(Lnet/minecraft/util/math/BlockPos;)Z"))
+//	private boolean nali_extra_renderEntities(WorldClient instance, BlockPos blockPos)
+//	{
+/// /		return instance.isBlockLoaded(blockPos);
+//		return true;
+//	}
+
+	@Inject(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderManager;renderEntityStatic(Lnet/minecraft/entity/Entity;FZ)V", ordinal = 1, shift = At.Shift.AFTER))
+	private void nali_extra_renderEntities(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo ci)
+	{
+		if (LAST == -2)
+		{
+			RenderO.take();
+			GL11.glEnable(GL11.GL_DEPTH_TEST);
+			GL11.glEnable(GL11.GL_BLEND);
+			GL20.glBlendEquationSeparate(GL14.GL_FUNC_ADD, GL14.GL_FUNC_ADD);
+			GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+
+			if (!Draw.MODEL_MAP.isEmpty())
+			{
+				Draw.draw(Draw.MODEL_MAP);
+			}
+
+			if ((Small.FLAG & 1) == 0)
+			{
+				if (!Draw.TRANSLUCENT_MAP.isEmpty())
+				{
+//					Draw.draw(Draw.TRANSLUCENT_MAP);
+					Draw.drawT();
+				}
+			}
+
+			RenderO.free();
+			Draw.clear();
+			LAST = -3;
+		}
+	}
+
+//	private static int PASS;
 //
-//import com.nali.small.draw.Draw;
-//import net.minecraft.client.renderer.RenderGlobal;
-//import net.minecraft.client.renderer.culling.ICamera;
-//import net.minecraft.entity.Entity;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//	@Inject(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos$PooledMutableBlockPos;release()V", shift = At.Shift.AFTER))
+//	private void nali_renderWorldR(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo ci)
+//	{
+//		PASS = net.minecraftforge.client.MinecraftForgeClient.getRenderPass();
 //
-////@Mixin(value = RenderGlobal.class, priority = Integer.MAX_VALUE)
-//@Mixin(RenderGlobal.class)
-//public abstract class MixinRenderGlobal
-//{
-////	private static int PASS;
-////
-////	@Inject(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos$PooledMutableBlockPos;release()V", shift = At.Shift.AFTER))
-////	private void nali_renderWorldR(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo ci)
-////	{
-////		PASS = net.minecraftforge.client.MinecraftForgeClient.getRenderPass();
-////
-////		if (PASS == 0)
-////		{
-//////			Nali.warn("0");
-////			Draw.runE();
-////		}
-////	}
-////
-////	@Inject(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderManager;setRenderOutlines(Z)V", shift = At.Shift.BEFORE, ordinal = 1))
-////	private void nali_renderWorldO(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo ci)
-////	{
-////		if (PASS == 0)
-////		{
-//////			Nali.warn("1");
-////			Draw.runEG();
-////		}
-////	}
-////
-////	@Inject(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;postRenderDamagedBlocks()V", shift = At.Shift.BEFORE))
-////	private void nali_renderWorldB(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo ci)
-////	{
-////		if (PASS == 0)
-////		{
-//////			Nali.warn("2");
-////			Draw.runT();
-////			Draw.clear();
-////		}
-////	}
-//}
+//		if (PASS == 0)
+//		{
+////			Nali.warn("0");
+//			Draw.runE();
+//		}
+//	}
+//
+//	@Inject(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderManager;setRenderOutlines(Z)V", shift = At.Shift.BEFORE, ordinal = 1))
+//	private void nali_renderWorldO(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo ci)
+//	{
+//		if (PASS == 0)
+//		{
+////			Nali.warn("1");
+//			Draw.runEG();
+//		}
+//	}
+//
+//	@Inject(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;postRenderDamagedBlocks()V", shift = At.Shift.BEFORE))
+//	private void nali_renderWorldB(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo ci)
+//	{
+//		if (PASS == 0)
+//		{
+////			Nali.warn("2");
+//			Draw.runT();
+//			Draw.clear();
+//		}
+//	}
+}
